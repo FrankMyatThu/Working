3bdf2e4e72979de6f391813b53986079db0c6ee2
myatthu1986 <myatthu1986@gmail.com>
2015-06-19 16:42:11 +0800

    add controller class and hello world test, but fail

---
+++ ASPNetMVC/GeekQuiz.SeperatedLayers/GeekQuizMultiLayer/Controller.GeekQuizMultiLayer/Controllers/TriviaController.cs
@@ -0,0 +1,25 @@
+﻿using Model.GeekQuizMultiLayer.BusinessLogic;
+using Model.GeekQuizMultiLayer.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Controller.GeekQuizMultiLayer.Controllers
+{
+    [Authorize]
+    public class TriviaController : ApiController
+    {
+        [AllowAnonymous]
+        public HttpResponseMessage Get() 
+        {
+            QuestionAnswer _QuestionAnswer = new QuestionAnswer();
+            TriviaQuestion _TriviaQuestion = _QuestionAnswer.GetQuestion("myatthu1986@yahoo.com");
+            string Title = _TriviaQuestion.Title;
+
+            return Request.CreateResponse(HttpStatusCode.OK, Title);          
+        }
+    }
+}
