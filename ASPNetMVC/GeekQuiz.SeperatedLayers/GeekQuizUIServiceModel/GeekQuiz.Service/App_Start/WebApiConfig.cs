7239823516f1e9ef18cc0c740b06a75621b5997c
myatthu1986 <myatthu1986@gmail.com>
2015-06-25 18:32:38 +0800

    add new style called UI_SERVICE_Model -now can post but have to check db error and async issues

---
+++ ASPNetMVC/GeekQuiz.SeperatedLayers/GeekQuizUIServiceModel/GeekQuiz.Service/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace GeekQuiz.Service
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            EnableCrossSiteRequests(config);
+            AddRoutes(config);
+        }
+
+        private static void AddRoutes(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "Default",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        private static void EnableCrossSiteRequests(HttpConfiguration config)
+        {
+            var cors = new EnableCorsAttribute(
+                origins: "*",
+                headers: "*",
+                methods: "*");
+            config.EnableCors(cors);
+        }
+    }
+}
---
+++ ASPNetMVC/GeekQuiz.SeperatedLayers/GeekQuizUIServiceModel/GeekQuiz.Service/Controllers/TriviaController.cs
@@ -0,0 +1,30 @@
+﻿using GeekQuiz.Model.BusinessLogic;
+using GeekQuiz.Model.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace GeekQuiz.Service.Controllers
+{   
+    public class TriviaController : ApiController
+    {        
+        public HttpResponseMessage Get()
+        {
+            QuestionAnswer _QuestionAnswer = new QuestionAnswer();
+            TriviaQuestion _TriviaQuestion = _QuestionAnswer.GetQuestion("myatthu1986@yahoo.com");
+            return Request.CreateResponse(HttpStatusCode.OK, _TriviaQuestion);
+        }
+
+        public HttpResponseMessage Post(TriviaAnswer _TriviaAnswer)
+        {
+            QuestionAnswer _QuestionAnswer = new QuestionAnswer();
+            bool IsCorrect = _QuestionAnswer.SetAnswer(_TriviaAnswer);
+            return Request.CreateResponse(HttpStatusCode.OK, IsCorrect);            
+        }
+        
+    }
+}
