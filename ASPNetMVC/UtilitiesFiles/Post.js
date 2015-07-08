e6e8d7fe2a23c28a787a30fe2b00d7eb590c117c
myatthu1986 <myatthu1986@gmail.com>
2015-07-08 17:08:59 +0800

    add UtilitiesFiles/Post.js

---
+++ ASPNetMVC/UtilitiesFiles/Post.js
@@ -0,0 +1,16 @@
+https://localhost:44302/api/accounts/create
+{
+	"email": "myatthu1986@yahoo.com",
+	"userName": "myatthu1986",
+	"Password" : "Pass@1word",
+	"ConfirmPassword" : "Pass@1word",
+	"FirstName" : "Myat",
+	"LastName" : "Thu"
+}
+
+https://localhost:44302/api/accounts/ChangePassword
+{
+	"OldPassword": "Pass@1word",
+	"NewPassword": "Pass@2word",
+	"ConfirmPassword": "Pass@2word"	
+}
