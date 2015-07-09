2328010d781617b59f7e07215eb24afd7d55bc8f
myatthu1986 <myatthu1986@gmail.com>
2015-07-09 16:13:24 +0800

    add sql file

---
+++ ASPNetMVC/UtilitiesFiles/AspNetUserTables.sql
@@ -0,0 +1,7 @@
+DELETE FROM	dbo.AspNetUsers WHERE id <> '12a8eef3-a2e1-4d58-8b82-fb3d757f421f'
+
+SELECT * FROM dbo.AspNetUsers 
+SELECT * FROM dbo.AspNetUserRoles 
+SELECT * FROM dbo.AspNetUserLogins 
+SELECT * FROM dbo.AspNetUserClaims 
+SELECT * FROM dbo.AspNetRoles 
