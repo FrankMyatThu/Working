bbaa14a8194eaa549237afe01fbd99e2628c02d1
myatthu1986 <myatthu1986@gmail.com>
2015-09-16 18:48:12 +0800

    add todolist

---
+++ ASPNetMVC/Framework/GeekQuiz/GeekQuiz.TokenService/Startup.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Cors;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace GeekQuiz.TokenService
+{
+    public class Startup
+    {
+        NameValueCollection AppSettings = ConfigurationManager.AppSettings;
+        string CorsOrigins = "cors:Origins";
+        string CorsHeaders = "cors:Headers";
+        string CorsMethods = "cors:Methods";
+
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration httpConfig = new HttpConfiguration();
+            ConfigureOAuthTokenGeneration(app);
+            ConfigureOAuthTokenConsumption(app);
+            ConfigureWebApi(httpConfig);
+
+            var corsPolicy = new EnableCorsAttribute(
+                            origins: AppSettings[CorsOrigins],
+                            headers: AppSettings[CorsHeaders],
+                            methods: AppSettings[CorsMethods]); 
+
+            // Enable CORS for ASP.NET Identity
+            app.UseCors(new CorsOptions
+            {
+                PolicyProvider = new CorsPolicyProvider
+                {
+                    PolicyResolver = request =>
+                        request.Path.Value == "/oauth" ?
+                        corsPolicy.GetCorsPolicyAsync(null, CancellationToken.None) :
+                        Task.FromResult<CorsPolicy>(null)
+                }
+            });
+
+            // Enable CORS for Web API
+            httpConfig.EnableCors(corsPolicy);
+
+            app.UseWebApi(httpConfig);
+        }
+
+        private void ConfigureOAuthTokenGeneration(IAppBuilder app)
+        {
+            // Configure the db context and user manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
+
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                //For Dev enviroment only (on production should be AllowInsecureHttp = false)
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/oauth/login"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new CustomOAuthProvider(),
+                AccessTokenFormat = new CustomJwtFormat("http://localhost:59822")
+            };
+
+            // OAuth 2.0 Bearer Access Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+        }
+
+        private void ConfigureOAuthTokenConsumption(IAppBuilder app)
+        {
+
+            var issuer = "http://localhost:59822";
+            string audienceId = ConfigurationManager.AppSettings["as:AudienceId"];
+            byte[] audienceSecret = TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["as:AudienceSecret"]);
+
+            // Api controllers with an [Authorize] attribute will be validated with JWT
+            app.UseJwtBearerAuthentication(
+                new JwtBearerAuthenticationOptions
+                {
+                    AuthenticationMode = AuthenticationMode.Active,
+                    AllowedAudiences = new[] { audienceId },
+                    IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
+                    {
+                        new SymmetricKeyIssuerSecurityTokenProvider(issuer, audienceSecret)
+                    }
+                });
+        }
+
+        private void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
