a0dfc1e7cd43c8d0742ace440a8ecccf68c75b43
myatthu1986 <myatthu1986@gmail.com>
2015-10-22 18:50:10 +0800

    user setting starting

--- ASPNetMVC/Framework/SG50/SG50.TokenService/Models/POCO/ApplicationUser.cs
+++ ASPNetMVC/Framework/SG50/SG50.TokenService/Models/POCO/ApplicationUser.cs
@@ -13,7 +13,10 @@ using System.Web;
 namespace SG50.TokenService.Models.POCO
 {
     public class ApplicationUser : IdentityUser
-    {   
+    {
+        #region User Profile Area
+
+        #region Step 1 (General Info)
         [MaxLength(100)]
         public string FirstName { get; set; }
 
@@ -21,11 +24,64 @@ namespace SG50.TokenService.Models.POCO
         public string LastName { get; set; }
 
         [Required]
-        public string SaltKey { get; set; }
+        public string SaltKey { get; set; }        
+        #endregion
 
-        /// Send via email.
-        public string PasswordResetToken { get; set; }
+        #region Step 2 (General Info)
+        [MaxLength(100)]
+        public string NickName { get; set; }
 
+        [MaxLength(255)]
+        public string Photo { get; set; }
+
+        [MaxLength]
+        public string CCMail { get; set; }
+
+        public DateTime? DOB { get; set; }
+
+        public bool IsReceivedEmail { get; set; }
+        #endregion
+
+        #region Step 3 (Location Info)
+        [MaxLength]
+        public string Fax { get; set; }
+
+        [MaxLength(255)]
+        public string Address { get; set; }
+
+        public int? PostalCode { get; set; }
+
+        [MaxLength(255)]
+        public string City { get; set; }
+
+        [MaxLength(36)]
+        public string CountryID { get; set; }
+        #endregion
+
+        #region Step 4 (Company Info)
+        [MaxLength(36)]
+        public string CompanyID { get; set; }
+
+        [MaxLength(36)]
+        public string BusinessUnitID { get; set; }
+        #endregion
+
+        #region Step 5 (Optional)
+        [MaxLength]
+        public string Remark { get; set; }
+        #endregion
+
+        #endregion
+
+        #region User Setting (Only administrator can configure)
+        [MaxLength(36)]
+        public string GroupID { get; set; }
+        public bool IsLocked { get; set; }
+        public bool IsEnable { get; set; }
+        public DateTime? LastLoginTime { get; set; }
+        #endregion
+
+        #region Default table attributes
         [Timestamp]
         public Byte[] ExecutedTime { get; set; }
         public bool IsActive { get; set; }
@@ -33,16 +89,9 @@ namespace SG50.TokenService.Models.POCO
         public string CreatedBy { get; set; }
         public DateTime? UpdateDate { get; set; }
         public string UpdateBy { get; set; }
+        #endregion
 
         public virtual IList<UsedPassword> UserUsedPassword { get; set; }
         public virtual IList<ActiveUser> ActiveUser { get; set; }
-
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
-        {
-            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
-            // Add custom user claims here
-
-            return userIdentity;
-        }
     }
 }
