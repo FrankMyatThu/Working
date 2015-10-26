79f35b3767f1780b4508e4016f05f5482d50f77c
frank.myatthu <myatthu1986@gmail.com>
2015-10-26 22:52:48 +0800

    finsih interview lambda select

--- Interview/LastJoinedEmployeeInEachDepartment/CRUD.App/WebApplication/WebApplication/Entity.aspx
+++ Interview/LastJoinedEmployeeInEachDepartment/CRUD.App/WebApplication/WebApplication/Entity.aspx
@@ -9,7 +9,80 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    
+        <table>
+            <tr>
+                <td>
+                    <!-- Flat Form Area -->
+                    <table>
+                        <tr>
+                            <td>Employee ID</td>
+                            <td>
+                                <asp:TextBox runat="server" ID="txtEmployeeID"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Employee Name</td>
+                            <td>
+                                <asp:TextBox runat="server" ID="txtEmployeeName"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Department ID</td>
+                            <td>
+                                <asp:TextBox runat="server" ID="txtDepartmentID"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Department Name</td>
+                            <td>
+                                <asp:TextBox runat="server" ID="txtDepartmentName"></asp:TextBox>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Join Date</td>
+                            <td>
+                                <input type="date" runat="server" id="txtJoinDate"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>
+                                <asp:Button ID="btnSubmit" runat="server" Text="Save" OnClick="btnSubmit_Click" />
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <!-- Listing Area -->
+                    <asp:GridView ID="grdEmployeeList" runat="server" AutoGenerateColumns="False" 
+                        DataKeyNames="EmployeeID">
+                        <Columns>
+                            <asp:BoundField DataField="EmployeeID" HeaderText="EmployeeID" />
+                            <asp:BoundField DataField="EmployeeName" HeaderText="EmployeeName" />
+                            <asp:BoundField DataField="DepartmentID" HeaderText="DepartmentID" />
+                            <asp:BoundField DataField="DepartmentName" HeaderText="DepartmentName" />
+                            <asp:BoundField DataField="JoinDate" HeaderText="JoinDate" />
+                            
+                            <asp:TemplateField>
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="btnEdit" runat="server" CommandArgument='<%#Eval("EmployeeID")%>' OnCommand="btnEdit_Command">Edit</asp:LinkButton>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+                            <asp:TemplateField>
+                                <ItemTemplate>
+                                    <asp:LinkButton ID="btnDelete" runat="server" CommandArgument='<%#Eval("EmployeeID")%>' OnCommand="btnDelete_Command">Delete</asp:LinkButton>
+                                </ItemTemplate>
+                            </asp:TemplateField>
+
+
+                        </Columns>
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+        
     </div>
     </form>
 </body>
--- Interview/LastJoinedEmployeeInEachDepartment/CRUD.App/WebApplication/WebApplication/Entity.aspx.cs
+++ Interview/LastJoinedEmployeeInEachDepartment/CRUD.App/WebApplication/WebApplication/Entity.aspx.cs
@@ -9,9 +9,199 @@ namespace WebApplication
 {
     public partial class Entity : System.Web.UI.Page
     {
+        #region Events
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                ResetFormControl();
+                BindGrid();
+            }
         }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            switch (btnSubmit.Text)
+            {
+                case "Save":
+                    Insert();
+                    break;
+
+                case "Update":
+                    Update();
+                    break;
+            }
+            ResetFormControl();
+            BindGrid();
+        }
+
+        protected void btnEdit_Command(object sender, CommandEventArgs e)
+        {
+            int EmployeeID = Convert.ToInt32(e.CommandArgument.ToString());
+            BindFormControl(SelectData(EmployeeID));
+            btnSubmit.Text = "Update";
+        }
+
+        protected void btnDelete_Command(object sender, CommandEventArgs e)
+        {
+            int EmployeeID = Convert.ToInt32(e.CommandArgument.ToString());
+            Delete(EmployeeID);
+            ResetFormControl();
+            BindGrid();
+        }
+        #endregion
+
+        #region Function
+
+        #region Binding
+        public void ResetFormControl()
+        {
+            txtEmployeeID.Text = "";
+            txtEmployeeName.Text = "";
+            txtDepartmentID.Text = "";
+            txtDepartmentName.Text = "";
+            txtJoinDate.Value = "";
+            btnSubmit.Text = "Save";
+        }
+        public void BindFormControl(List<tbl_EmpDep> List_tbl_EmpDep)
+        {
+            foreach (tbl_EmpDep _tbl_EmpDep in List_tbl_EmpDep)
+            {
+                txtEmployeeID.Text = _tbl_EmpDep.EmployeeID.ToString();
+                txtEmployeeName.Text = _tbl_EmpDep.EmployeeName.ToString();
+                txtDepartmentID.Text = _tbl_EmpDep.DepartmentID.ToString();
+                txtDepartmentName.Text = _tbl_EmpDep.DepartmentName.ToString();
+                txtJoinDate.Value = Convert.ToDateTime(_tbl_EmpDep.JoinDate.ToString()).ToString("yyyy-MM-dd");
+            }
+        }
+        public void BindGrid()
+        {
+            grdEmployeeList.DataSource = SelectData();
+            grdEmployeeList.DataBind();
+        }
+        #endregion
+
+        #region CRUD
+        public List<tbl_EmpDep> SelectData(int EmployeeID = 0)
+        {
+            using (InterviewDBEntities _InterviewDBEntities = new InterviewDBEntities()) {
+
+                var List_tblEmpDep_RowNo = _InterviewDBEntities.tbl_EmpDep
+                                                            .ToList()
+                                                            .OrderByDescending(x => x.JoinDate)
+                                                            .GroupBy(x => x.DepartmentID)
+                                                            .Select(group => new { Group = group, Count = group.Count() })                                                            
+                                                            .SelectMany(groupWithCount =>
+                                                                groupWithCount.Group.Select(b => b)
+                                                                .Zip(
+                                                                    Enumerable.Range(1, groupWithCount.Count),
+                                                                    (j, i) => new { j.EmployeeID,
+                                                                                    j.EmployeeName,
+                                                                                    j.DepartmentID,
+                                                                                    j.DepartmentName,
+                                                                                    j.JoinDate,
+                                                                                    RowNumber = i 
+                                                                    }
+                                                                )
+                                                            );
+
+                List<tbl_EmpDep> List_tbl_EmpDep = List_tblEmpDep_RowNo.Where(x => x.RowNumber.Equals(1))
+                                                                                 .OrderBy(x => x.DepartmentID)
+                                                                                 .Select(x => new tbl_EmpDep
+                                                                                 {
+                                                                                     EmployeeID = x.EmployeeID,
+                                                                                     EmployeeName = x.EmployeeName,
+                                                                                     DepartmentID = x.DepartmentID,
+                                                                                     DepartmentName = x.DepartmentName,
+                                                                                     JoinDate = x.JoinDate
+                                                                                 }).ToList();
+
+                return List_tbl_EmpDep;
+            }
+        }
+
+        public void Insert()
+        {
+            //int EmployeeID = Convert.ToInt32(txtEmployeeID.Text.Trim());
+            //string EmployeeName = txtEmployeeName.Text.Trim();
+            //int DepartmentID = Convert.ToInt32(txtDepartmentID.Text.Trim());
+            //string DepartmentName = txtDepartmentName.Text.Trim();
+            //DateTime JoinDate = Convert.ToDateTime(txtJoinDate.Value.Trim());
+
+            //string SqlCommandText = " INSERT INTO tbl_EmpDep (EmployeeID,EmployeeName,DepartmentID,DepartmentName,JoinDate) " +
+            //                            " VALUES (@EmployeeID, @EmployeeName, @DepartmentID, @DepartmentName, @JoinDate) ";
+
+            //using (SqlConnection _SqlConnection = new SqlConnection(ConnetionString))
+            //{
+            //    using (SqlCommand _SqlCommand = new SqlCommand(SqlCommandText, _SqlConnection))
+            //    {
+            //        _SqlCommand.CommandType = CommandType.Text;
+            //        _SqlCommand.CommandText = SqlCommandText;
+            //        _SqlCommand.Parameters.Add("@EmployeeID", SqlDbType.Int).Value = EmployeeID;
+            //        _SqlCommand.Parameters.Add("@EmployeeName", SqlDbType.VarChar, 100).Value = EmployeeName;
+            //        _SqlCommand.Parameters.Add("@DepartmentID", SqlDbType.Int).Value = DepartmentID;
+            //        _SqlCommand.Parameters.Add("@DepartmentName", SqlDbType.VarChar, 100).Value = DepartmentName;
+            //        _SqlCommand.Parameters.Add("@JoinDate", SqlDbType.DateTime).Value = JoinDate;
+
+            //        _SqlConnection.Open();
+            //        _SqlCommand.ExecuteNonQuery();
+            //    }
+            //}
+        }
+
+        public void Update()
+        {
+            //int EmployeeID = Convert.ToInt32(txtEmployeeID.Text.Trim());
+            //string EmployeeName = txtEmployeeName.Text.Trim();
+            //int DepartmentID = Convert.ToInt32(txtDepartmentID.Text.Trim());
+            //string DepartmentName = txtDepartmentName.Text.Trim();
+            //DateTime JoinDate = Convert.ToDateTime(txtJoinDate.Value.Trim());
+
+            //string SqlCommandText = " UPDATE tbl_EmpDep " +
+            //                            " SET  " +
+            //                            "     EmployeeID = @EmployeeID,  " +
+            //                            "     EmployeeName = @EmployeeName, " +
+            //                            "     DepartmentID = @DepartmentID, " +
+            //                            "     DepartmentName = @DepartmentName, " +
+            //                            "     JoinDate = @JoinDate " +
+            //                            " WHERE EmployeeID = @EmployeeID ";
+
+            //using (SqlConnection _SqlConnection = new SqlConnection(ConnetionString))
+            //{
+            //    using (SqlCommand _SqlCommand = new SqlCommand(SqlCommandText, _SqlConnection))
+            //    {
+            //        _SqlCommand.CommandType = CommandType.Text;
+            //        _SqlCommand.CommandText = SqlCommandText;
+            //        _SqlCommand.Parameters.Add("@EmployeeID", SqlDbType.Int).Value = EmployeeID;
+            //        _SqlCommand.Parameters.Add("@EmployeeName", SqlDbType.VarChar, 100).Value = EmployeeName;
+            //        _SqlCommand.Parameters.Add("@DepartmentID", SqlDbType.Int).Value = DepartmentID;
+            //        _SqlCommand.Parameters.Add("@DepartmentName", SqlDbType.VarChar, 100).Value = DepartmentName;
+            //        _SqlCommand.Parameters.Add("@JoinDate", SqlDbType.DateTime).Value = JoinDate;
+
+            //        _SqlConnection.Open();
+            //        _SqlCommand.ExecuteNonQuery();
+            //    }
+            //}
+        }
+
+        public void Delete(int EmployeeID)
+        {
+            //string SqlCommandText = "DELETE FROM tbl_EmpDep WHERE EmployeeID = @EmployeeID";
+
+            //using (SqlConnection _SqlConnection = new SqlConnection(ConnetionString))
+            //{
+            //    using (SqlCommand _SqlCommand = new SqlCommand(SqlCommandText, _SqlConnection))
+            //    {
+            //        _SqlCommand.CommandType = CommandType.Text;
+            //        _SqlCommand.CommandText = SqlCommandText;
+            //        _SqlCommand.Parameters.Add("@EmployeeID", SqlDbType.Int).Value = EmployeeID;
+            //        _SqlConnection.Open();
+            //        _SqlCommand.ExecuteNonQuery();
+            //    }
+            //}
+        }
+        #endregion
+
+        #endregion
     }
 }
