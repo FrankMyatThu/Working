5a20d4cc66f14a3915cb76fd0cd521685b93d451
frank.myatthu <myatthu1986@gmail.com>
2015-10-30 23:02:17 +0800

    just start mvc interview project

---
+++ Interview/LastJoinedEmployeeInEachDepartment/CRUD.App/WebApplication/MVCWebApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,64 @@
+﻿using MVCWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<tbl_EmpDep> List_tbl_EmpDep = new List<tbl_EmpDep>(); 
+            using (InterviewDBEntities _InterviewDBEntities = new InterviewDBEntities())
+            {
+                List_tbl_EmpDep = _InterviewDBEntities.tbl_EmpDep.Select(x => x).ToList<tbl_EmpDep>();
+            }
+
+            return View(List_tbl_EmpDep);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
