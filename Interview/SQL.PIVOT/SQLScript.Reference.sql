966fd1125ea22825cee66379bb9513bf126320d1
frank.myatthu <myatthu1986@gmail.com>
2015-11-01 12:12:22 +0800

    before answer looping sql

--- Interview/SQL.PIVOT/SQLScript.Reference.sql
+++ Interview/SQL.PIVOT/SQLScript.Reference.sql
@@ -5,7 +5,7 @@ USE InterviewDB
 CREATE TABLE tbl_FlatTable
 (
 	EmployeeName VARCHAR(100) NOT NULL,
-	JoinDate DATETIME,
+	ConfirmedDate DATETIME,
 	Field_Type VARCHAR(100) NOT NULL,
 	Field_Value VARCHAR(100) NOT NULL
 )
@@ -14,7 +14,7 @@ CREATE TABLE tbl_FlatTable
 -- Insert 
 -- ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 INSERT INTO tbl_FlatTable 
-(EmployeeName,JoinDate,Field_Type,Field_Value)
+(EmployeeName,ConfirmedDate,Field_Type,Field_Value)
 VALUES 
 ('John', '2009/01/01', 'Title', 'Programmer'), 
 ('John', '2009/01/01', 'Department', 'IT Department'), 
@@ -24,8 +24,28 @@ VALUES
 -- ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- select table using PIVOT function
 -- ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+;WITH _tbl_FlatTable AS
+(
+	SELECT *, 
+		MAX(ConfirmedDate) OVER (PARTITION BY EmployeeName, Field_Type) AS MAXDate_OVER_EMP_FIELDTYPE,
+		MAX(ConfirmedDate) OVER (PARTITION BY EmployeeName) AS MAXDate_OVER_EMP
+	FROM tbl_FlatTable
+)
 
-
+SELECT ConfirmedDate = MAXDate_OVER_EMP, EmployeeName, Title, Department
+FROM
+(
+	SELECT 
+		EmployeeName,
+		MAXDate_OVER_EMP,
+		Field_Type,
+		Field_Value
+	FROM 
+		_tbl_FlatTable
+	WHERE ConfirmedDate = MAXDate_OVER_EMP_FIELDTYPE 	-- /// This line kick out junior programmer row.
+) _tbl_FlatTable
+PIVOT (MAX(Field_Value) FOR  Field_Type IN (Title, Department)) AS _PIVOT
+ORDER BY ConfirmedDate DESC
 -- ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Select All 
 -- ---------------------------------------------------------------------------------------------------------------------------------------------------------------
