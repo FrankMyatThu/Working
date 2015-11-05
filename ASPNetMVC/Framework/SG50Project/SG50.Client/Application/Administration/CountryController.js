65d8c32855a63fd6639e7a0ffad2cb85968ebade
myatthu1986 <myatthu1986@gmail.com>
2015-11-05 10:57:11 +0800

    backup grid binding

--- ASPNetMVC/Framework/SG50Project/SG50.Client/Application/Administration/CountryController.js
+++ ASPNetMVC/Framework/SG50Project/SG50.Client/Application/Administration/CountryController.js
@@ -1,4 +1,4 @@
-﻿app.controller('CountryController', function ($scope, $http, $window, $q) {
+﻿app.controller('CountryController', function ($scope, $http, $window) {
     $scope.DisplayData = "";
     $scope.gridOptions = {};
     $scope.gridOptions.columnDefs = [
@@ -9,8 +9,7 @@
     //$scope.gridOptions.data = [{ "Id": "0735aca6-d4fa-40f0-ba7c-000044a0600c", "Name": "Test Data 124870 value" }, { "Id": "4f5b3d6e-6753-48e7-b3fc-0000a10a7df5", "Name": "Test Data 113034 value" }, { "Id": "c722c447-fb33-45dd-b4e9-0000fc5c1b5c", "Name": "Test Data 164240 value" }, { "Id": "c9fb67ff-c178-4ff7-bb51-0002b92f8825", "Name": "Test Data 43956 value" }, { "Id": "2b39f982-ce0c-4314-8101-000313083c94", "Name": "Test Data 57350 value" }, { "Id": "2a3eea10-96c5-455a-bc01-000454ec8674", "Name": "Test Data 98202 value" }, { "Id": "7730db34-6b71-4405-8adb-0004d22c563e", "Name": "Test Data 151804 value" }, { "Id": "1036b382-c586-4665-abb1-00059b42561b", "Name": "Test Data 17865 value" }, { "Id": "3f21d02f-25a7-419e-b19c-00061767dc3e", "Name": "Test Data 71344 value" }, { "Id": "69912479-51cc-43af-b186-000620ccfdb8", "Name": "Test Data 3873 value" }];
 
     $scope.init = function () {
-        console.log("$scope.init...");
-        var deferred = $q.defer();
+        console.log("$scope.init...");        
         $http({
             method: 'POST',
             url: ApplicationConfig.Service_Domain.concat(ApplicationConfig.Service_GetCountryList),
@@ -27,14 +26,12 @@
                 }
                 console.log(str);
             }
-            else {
-                deferred.resolve(data);
+            else {                
                 $scope.gridOptions.data = data;
                 //$scope.DisplayData = data;
                 //$scope.dataLoading = false;
             }
-        }).error(function (data, status, headers, config) {
-            deferred.reject;
+        }).error(function (data, status, headers, config) {            
             var ErrorMessageValue = "";
             var ExceptionMessageValue = "";
             ErrorNotifier(data);
@@ -57,8 +54,7 @@
                 //$scope.error = ErrorMessageValue;
             }
             //$scope.dataLoading = false;
-        });
-        return deferred.promise;
+        });        
     };
 
     //angular.element(document).ready(function () {
