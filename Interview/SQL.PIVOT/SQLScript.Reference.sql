f79d0b247f82c903007d58beed5084e02a231375
frank.myatthu <myatthu1986@gmail.com>
2015-12-12 22:51:07 +0800

    update pivot references

--- Interview/SQL.PIVOT/SQLScript.Reference.sql
+++ Interview/SQL.PIVOT/SQLScript.Reference.sql
@@ -26,25 +26,26 @@ VALUES
 -- ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 ;WITH _tbl_FlatTable AS
 (
-	SELECT *, 
-		MAX(ConfirmedDate) OVER (PARTITION BY EmployeeName, Field_Type) AS MAXDate_OVER_EMP_FIELDTYPE,
-		MAX(ConfirmedDate) OVER (PARTITION BY EmployeeName) AS MAXDate_OVER_EMP
-	FROM tbl_FlatTable
+	SELECT *,
+	MAX(tbl_FlatTable.ConfirmedDate) OVER (PARTITION BY tbl_FlatTable.EmployeeName, tbl_FlatTable.Field_Type) AS Max_Date_By_Title,
+	MAX(tbl_FlatTable.ConfirmedDate) OVER (PARTITION BY tbl_FlatTable.EmployeeName) AS Max_Date_By_Name
+	FROM tbl_FlatTable 
 )
-
-SELECT ConfirmedDate = MAXDate_OVER_EMP, EmployeeName, Title, Department
-FROM
-(
+SELECT 
+	_PIVOT.Max_Date_By_Name AS ConfirmedDate,  -- /// This line is fake ConfirmDate because different ConfirmedDate in same group (PARTITION BY EmployeeName) can create more than one record(s) using null.
+	_PIVOT.EmployeeName, 
+	_PIVOT.Title, 
+	_PIVOT.Department  
+FROM (
 	SELECT 
-		EmployeeName,
-		MAXDate_OVER_EMP,
-		Field_Type,
-		Field_Value
-	FROM 
-		_tbl_FlatTable
-	WHERE ConfirmedDate = MAXDate_OVER_EMP_FIELDTYPE 	-- /// This line kick out junior programmer row.
-) _tbl_FlatTable
-PIVOT (MAX(Field_Value) FOR  Field_Type IN (Title, Department)) AS _PIVOT
+		Max_Date_By_Name, 
+		EmployeeName, 
+		Field_Type, 
+		Field_Value 
+	FROM _tbl_FlatTable 
+	WHERE ConfirmedDate = Max_Date_By_Title -- /// This line kick out junior programmer row.
+) _UpSizeDown
+PIVOT (MAX(Field_Value) FOR Field_Type IN (Department, Title) ) AS _PIVOT
 ORDER BY ConfirmedDate DESC
 -- ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Select All 
