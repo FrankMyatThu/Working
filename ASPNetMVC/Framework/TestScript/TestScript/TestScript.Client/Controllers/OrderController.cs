e3eb7945d2bd86f66e000aec36f3b7ba4ddf0d70
myatthu1986 <myatthu1986@gmail.com>
2016-01-29 15:39:27 +0800

    success blank list MVC

---
+++ ASPNetMVC/Framework/TestScript/TestScript/TestScript.Client/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestScript.Client.Controllers
+{
+    public class OrderController : Controller
+    {
+        #region Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Retrieve
+        public ActionResult List()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Update
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Delete/Detail
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
