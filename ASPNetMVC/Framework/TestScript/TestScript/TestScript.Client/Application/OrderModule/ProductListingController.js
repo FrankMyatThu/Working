6b9924f4b2ca2bbefc2e9b47bcd904ac23b10157
frank.myatthu <myatthu1986@gmail.com>
2016-01-29 23:00:14 +0800

    ProductListingController.js Line 166 $scope.Detail still checking

---
+++ ASPNetMVC/Framework/TestScript/TestScript/TestScript.Client/Application/OrderModule/ProductListingController.js
@@ -0,0 +1,405 @@
+﻿//#region factory
+app.factory('productListingDataFactory', function ($http) {
+    var productListingDataFactory = {
+        selectProduct: selectProduct,
+        deleteCountry: deleteCountry
+    };
+    return productListingDataFactory;
+
+    //////////////////////////////////
+
+    function selectProduct(_Product_Criteria_Model) {
+        return $http({
+            method: 'POST',
+            url: ApplicationConfig.Service_Domain.concat(ApplicationConfig.Service_Product_GetProduct),
+            //headers: {
+            //    'accept': 'application/json; charset=utf-8',
+            //    'Authorization': 'Bearer ' + sessionStorage.getItem("JWTToken"),
+            //    'RequestVerificationToken': ApplicationConfig.AntiForgeryTokenKey
+            //},
+            data: _Product_Criteria_Model
+        });
+    };
+
+    function deleteCountry(_Product_Criteria_Model) {
+        return $http({
+            method: 'POST',
+            url: ApplicationConfig.Service_Domain.concat(ApplicationConfig.Service_DeleteCountryList),
+            //headers: {
+            //    'accept': 'application/json; charset=utf-8',
+            //    'Authorization': 'Bearer ' + sessionStorage.getItem("JWTToken"),
+            //    'RequestVerificationToken': ApplicationConfig.AntiForgeryTokenKey
+            //},
+            data: _Product_Criteria_Model
+        });
+    };
+});
+//#endregion
+
+//#region filter
+app.filter('offset', function () {
+    return function (input, start) {
+        if (!input || !input.length) { return; }
+        start = parseInt(start, 10);        
+        return input.slice(start);
+    };
+});
+//#endregion
+
+//#region controller
+//#region Controller for searching popup dialog box.
+app.controller('Modal_PopupSearch_Controller', function ($scope, $uibModalInstance) {
+
+    $scope.Search = function () {
+        $uibModalInstance.close();
+    };
+
+    $scope.Cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+
+});
+//#endregion
+//#region Controller for listing country info.
+app.controller('ProductListingController', function ($scope, $http, $window, $uibModal, $timeout, productListingDataFactory) {
+
+    //#region Initial declaration        
+    $scope.IsRecordFound = true;
+    $scope.currentPage = 0;
+    // Data to populate grid.
+    $scope.items = [];
+    $scope.itemsLength = 0;
+    // Data to pupulate pager.
+    $scope.List_tbl_Pager_To_Client_ByBatchIndex = {};
+    // Criteria to search in database.
+    $scope.Product_Criteria_Model = {
+        // -- Pager --
+        "BatchIndex": 1,
+        "PagerShowIndexOneUpToX": 10,
+        "RecordPerPage": 10,
+        "RecordPerBatch": 100,
+
+        // -- Sorting --
+        "OrderByClause": "ProductName ASC",
+
+        // -- Data --
+        "SrNo": "",
+        "ProductID": "",
+        "ProductName": "",
+        "Description": "",
+        "Price": ""
+    };
+    // Sorting criteria.
+    $scope.sort = {
+        sortingOrder: 'ProductName',
+        reverse: false
+    };
+    // Checkbox Control
+    $scope.checkboxControl = {
+        "IsSelectedAll": false,
+        "IsSelectedAllTotally": false,
+        "SelectItemList": {},
+        "currentDisplayedPageRecord": {},
+        "currentDisplayedPageRecord_Total": 0
+    };
+    // Delete
+    $scope.Delete = {
+        "IsFinishDeleted": false,
+        "DeletedRowCount": 0
+    };
+    //#endregion
+
+    //#region Reset criteria array
+    $scope.Reset_Product_Criteria_Model_Data = function () {
+        $scope.Product_Criteria_Model.SrNo = "";
+        $scope.Product_Criteria_Model.ProductID = "";
+        $scope.Product_Criteria_Model.ProductName = "";
+        $scope.Product_Criteria_Model.Description = "";
+        $scope.Product_Criteria_Model.Price = "";        
+    }
+    //#endregion
+
+    //#region Grid's Checkbox 
+    $scope.ResetCheckBoxControl = function (value) {
+        $scope.checkboxControl.IsSelectedAll = value;
+        $scope.checkboxControl.IsSelectedAllTotally = false;
+        $scope.checkboxControl.SelectItemList = {};
+        $scope.checkboxControl.currentDisplayedPageRecord = {};
+    };
+    $scope.selectAll = function () {
+        $scope.ResetCheckBoxControl($scope.checkboxControl.IsSelectedAll);
+        $scope.checkboxControl.currentDisplayedPageRecord = $scope.items.slice(0);
+        $scope.checkboxControl.currentDisplayedPageRecord = $scope.checkboxControl.currentDisplayedPageRecord.splice($scope.currentPage * $scope.Product_Criteria_Model.RecordPerPage, $scope.Product_Criteria_Model.RecordPerPage);
+        for (var i = 0; i < $scope.checkboxControl.currentDisplayedPageRecord.length; i++) {
+            var item = $scope.checkboxControl.currentDisplayedPageRecord[i];
+            $scope.checkboxControl.SelectItemList[item.ProductID] = $scope.checkboxControl.IsSelectedAll;
+        }
+        $scope.checkboxControl.currentDisplayedPageRecord_Total = $scope.checkboxControl.currentDisplayedPageRecord.length;
+    };
+    $scope.selectAllTotally = function (value) {        
+        $scope.checkboxControl.IsSelectedAllTotally = value;
+    };
+    $scope.voidSelection = function (){
+        $scope.ResetCheckBoxControl(false);
+    }
+    $scope.checkboxStateChanged = function (ProductID) {
+        if ($scope.checkboxControl.SelectItemList[ProductID]) {
+            /// Checked  
+            //console.log("Checked", ProductID);
+        } else {
+            /// Not checked
+            $scope.checkboxControl.IsSelectedAll = false;
+        }        
+    }
+    //#endregion
+
+    //#region Edit
+    $scope.Edit = function (ProductID) {
+        console.log("ProductID", ProductID);
+        sessionStorage.setItem("ProductID", ProductID);        
+        location.href = ApplicationConfig.Client_Domain.concat(ApplicationConfig.Client_Country_Edit);
+    };
+    //#endregion
+
+    //#region Detail
+    $scope.Detail = function (ProductID) {
+        console.log("ProductID", ProductID);
+        sessionStorage.setItem("ProductID", ProductID);
+        location.href = ApplicationConfig.Client_Domain.concat(ApplicationConfig.Client_Country_Detail);
+    };
+    //#endregion
+
+    //#region Create New
+    $scope.newPage = function () {
+        location.href = ApplicationConfig.Client_Domain.concat(ApplicationConfig.Client_Country_Create);
+    }
+    //#endregion
+
+    //#region Delete    
+    $scope.deletePage = function () {        
+        var result = confirm("Are you sure to delete?");
+        if (!result) {            
+            return;
+        }
+        if ($scope.checkboxControl.IsSelectedAllTotally) {
+            var List_Product_Criteria_Model = [];
+            List_Product_Criteria_Model.push($scope.Product_Criteria_Model);
+            productListingDataFactory.deleteCountry(List_Product_Criteria_Model)
+            .success(function (data, status, headers, config) {
+                $scope.Reset_Product_Criteria_Model_Data();
+                $scope.Delete.DeletedRowCount = $scope.itemsLength;
+                $scope.firstPage();                
+                $scope.Delete.IsFinishDeleted = true;                
+                $timeout(function () { $scope.Delete.IsFinishDeleted = false; }, 4000);
+            }).error(function (data, status, headers, config) {
+                $scope.errorHandler(data, status, headers, config);               
+            });
+            
+        }
+        else {
+            var List_Product_Criteria_Model = [];
+            $scope.Reset_Product_Criteria_Model_Data();
+            angular.forEach($scope.checkboxControl.SelectItemList, function (value, key) {                
+                if (value) {
+                    var _Product_Criteria_Model = angular.copy($scope.Product_Criteria_Model);
+                    _Product_Criteria_Model.ProductID = key;
+                    List_Product_Criteria_Model.push(_Product_Criteria_Model);
+                }
+            });           
+            if (List_Product_Criteria_Model.length <= 0) {
+                alert("Please select item(s) which you want to delete.");
+                $scope.firstPage();
+                return;
+            }
+            productListingDataFactory.deleteCountry(List_Product_Criteria_Model)
+            .success(function (data, status, headers, config) {
+                $scope.Delete.DeletedRowCount = List_Product_Criteria_Model.length;
+                $scope.firstPage();
+                $scope.Delete.IsFinishDeleted = true;                
+                $timeout(function () { $scope.Delete.IsFinishDeleted = false; }, 4000);
+            }).error(function (data, status, headers, config) {
+                $scope.errorHandler(data, status, headers, config);               
+            });            
+        }
+    };
+    //#endregion
+
+    //#region Grid's Searching
+    $scope.searchPage = function (size) {        
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'ModalContent_PopupSearch.html',
+            controller: 'Modal_PopupSearch_Controller',
+            scope: $scope,
+            size: size,
+        });
+
+        modalInstance.result.then(function () {
+            $scope.Product_Criteria_Model.BatchIndex = 1;
+            productListingDataFactory.selectProduct($scope.Product_Criteria_Model)
+            .success(function (data, status, headers, config) {
+                $scope.dataOptimizer(data);
+                $scope.currentPage = 0;
+            }).error(function (data, status, headers, config) {
+                $scope.errorHandler(data, status, headers, config);
+            });
+        }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+        });
+
+    };
+    //#endregion
+
+    //#region Pager
+    $scope.firstPage = function () {
+        $scope.Product_Criteria_Model.BatchIndex = 1;
+        productListingDataFactory.selectProduct($scope.Product_Criteria_Model)
+        .success(function (data, status, headers, config) {
+            $scope.dataOptimizer(data);
+            $scope.currentPage = 0;
+        }).error(function (data, status, headers, config) {
+            $scope.errorHandler(data, status, headers, config);
+        });
+    }
+
+    $scope.lastPage = function () {
+        $scope.Product_Criteria_Model.BatchIndex = Math.ceil($scope.itemsLength / $scope.Product_Criteria_Model.RecordPerBatch);
+        productListingDataFactory.selectProduct($scope.Product_Criteria_Model)
+        .success(function (data, status, headers, config) {
+            $scope.dataOptimizer(data);
+            $scope.currentPage = $scope.List_tbl_Pager_To_Client_ByBatchIndex.length - 1;
+        }).error(function (data, status, headers, config) {
+            $scope.errorHandler(data, status, headers, config);
+        });
+    };
+
+    $scope.prevPage = function () {
+        if ($scope.Product_Criteria_Model.BatchIndex > 1) {
+            $scope.Product_Criteria_Model.BatchIndex--;
+            productListingDataFactory.selectProduct($scope.Product_Criteria_Model)
+            .success(function (data, status, headers, config) {
+                $scope.dataOptimizer(data);
+                $scope.currentPage = $scope.Product_Criteria_Model.PagerShowIndexOneUpToX - 1;
+            }).error(function (data, status, headers, config) {
+                $scope.errorHandler(data, status, headers, config);
+            });
+        }
+    };
+
+    $scope.prevPageDisabled = function () {
+        return $scope.Product_Criteria_Model.BatchIndex === 1 ? "disabled" : "";
+    };
+
+    $scope.nextPage = function () {
+        if ($scope.Product_Criteria_Model.BatchIndex < Math.ceil($scope.itemsLength / $scope.Product_Criteria_Model.RecordPerBatch)) {
+            $scope.Product_Criteria_Model.BatchIndex++;
+            productListingDataFactory.selectProduct($scope.Product_Criteria_Model)
+            .success(function (data, status, headers, config) {
+                $scope.dataOptimizer(data);
+                $scope.currentPage = 0;
+            }).error(function (data, status, headers, config) {
+                $scope.errorHandler(data, status, headers, config);
+            });
+        }
+    };
+
+    $scope.nextPageDisabled = function () {
+        return $scope.Product_Criteria_Model.BatchIndex === Math.ceil($scope.itemsLength / $scope.Product_Criteria_Model.RecordPerBatch) ? "disabled" : "";
+    };
+
+    $scope.setPage = function (n) {
+        $scope.currentPage = n;
+        $scope.ResetCheckBoxControl(false);
+    };
+    //#endregion
+
+    //#region Optimize data after Ajax request
+    $scope.dataOptimizer = function (data) {
+        $scope.ResetCheckBoxControl(false);
+        if (data[0].List_T.length <= 0) {
+            $scope.IsRecordFound = false;
+            return;
+        }
+        $scope.items = data[0].List_T;
+        $scope.itemsLength = data[0].List_T[0].TotalRecordCount;
+        $scope.List_tbl_Pager_To_Client_ByBatchIndex = data[0].List_tbl_Pager_To_Client.filter(function (item) { return item.BatchIndex === $scope.Product_Criteria_Model.BatchIndex; });
+    };
+    //#endregion
+    
+});
+//#endregion
+//#endregion
+
+//#region directive
+app.directive("customSort", function () {
+    return {
+        restrict: 'A',
+        transclude: true,
+        scope: true,
+        template:
+          '<a href="#" ng-click="sort_by(order)" style="color: #555555;">' +
+          '    <span ng-transclude></span>' +
+          '    <i ng-class="selectedCls(order)"></i>' +
+          '</a>',
+        link: function (scope, elem, attrs) {
+            scope.order = attrs.order;
+
+            scope.sort_by = function (newSortingOrder) {
+                var sort = scope.sort;
+                if (sort.sortingOrder == newSortingOrder) {
+                    sort.reverse = !sort.reverse;                    
+                }
+
+                sort.sortingOrder = newSortingOrder;
+                scope.Product_Criteria_Model.OrderByClause = sort.sortingOrder + ((scope.sort.reverse) ? ' DESC' : ' ASC');
+                scope.firstPage();
+            };
+
+            scope.selectedCls = function (column) {
+                if (column == scope.sort.sortingOrder) {
+                    return ('fa fa-chevron-' + ((scope.sort.reverse) ? 'down' : 'up'));
+                }
+                else {
+                    return 'fa fa-sort'
+                }
+            };
+        }
+    }
+});
+
+app.directive("ddlRecordFilterCount", ['productListingDataFactory', function (productListingDataFactory) {
+    return {
+        restrict: 'A',
+        transclude: true,
+        scope: false,
+        template:
+            ' Filter By&nbsp;<span class="glyphicon glyphicon-filter"></span> ' +
+            ' <select ProductID="ddlRecordFilterCount" name="ddlRecordFilterCount"  ' +
+            ' ng-options="option.name for option in data.availableOptions track by option.ProductID" ng-model="data.selectedOption" ' +
+            ' ng-change="ddlRecordFilterCount_Change(data.selectedOption)" ></select> ',
+        link: function (scope, elem, attrs) {
+            scope.data = {
+                availableOptions: [
+                    { ProductID: '10', name: '10' },
+                    { ProductID: '20', name: '20' },
+                    { ProductID: '50', name: '50' }
+                ],
+                selectedOption: { ProductID: '10', name: '10' }
+            };
+            scope.ddlRecordFilterCount_Change = function (SelectedValue) {
+                scope.Product_Criteria_Model.BatchIndex = 1;
+                scope.Product_Criteria_Model.RecordPerPage = SelectedValue.ProductID;
+                scope.Product_Criteria_Model.RecordPerBatch = scope.Product_Criteria_Model.RecordPerPage * scope.Product_Criteria_Model.PagerShowIndexOneUpToX;
+                productListingDataFactory.selectProduct(scope.Product_Criteria_Model)
+                .success(function (data, status, headers, config) {
+                    scope.dataOptimizer(data);
+                    scope.currentPage = 0;
+                }).error(function (data, status, headers, config) {                    
+                    scope.errorHandler(data, status, headers, config);
+                });
+            };
+        }
+    }
+}]);
+//#endregion
