8127326954b797f7f965628e8f67c57704c285cb
myatthu1986 <myatthu1986@gmail.com>
2016-02-26 18:43:06 +0800

    update answer

--- Interview/AlgorithmTesterProject/AlgorithmTesterProject/WinForm/Form1.cs
+++ Interview/AlgorithmTesterProject/AlgorithmTesterProject/WinForm/Form1.cs
@@ -28,17 +28,51 @@ namespace WinForm
         /// </summary>
         /// <param name="A"></param>
         /// <returns></returns>
-        public int[] solution(int[] A)
-        {
-            List<int> List_ReturnInt = new List<int>();
-            if (A.Length <= 0) {
-                List_ReturnInt.Add(-1);
-                return List_ReturnInt.ToArray(); 
+        //public int[] solution(int[] A)
+        //{
+        //    List<int> List_ReturnInt = new List<int>();
+        //    if (A.Length <= 0) {
+        //        List_ReturnInt.Add(-1);
+        //        return List_ReturnInt.ToArray(); 
+        //    }
+
+        //    int LeftSumValue = 0;
+        //    int RightSumValue = 0;
+            
+        //    /// Get right sum total.
+        //    for (int i = 0; i < A.Length; i++)
+        //    {
+        //        RightSumValue += A[i];
+        //    }
+
+        //    /// Get left value and compare
+        //    for (int i = 0; i < A.Length; i++) 
+        //    {
+        //        int CurrentValue = RightSumValue - A[i];
+        //        if (LeftSumValue == CurrentValue)
+        //        {
+        //            /// found equilibrium
+        //            List_ReturnInt.Add(i);
+        //        }
+        //        else 
+        //        {
+        //            LeftSumValue += A[i];
+        //            RightSumValue = CurrentValue;
+        //        }
+        //    }
+        //    return List_ReturnInt.ToArray();
+        //}
+
+        public int solution(int[] A)
+        {   
+            if (A.Length <= 0)
+            {   
+                return -1;
             }
 
             int LeftSumValue = 0;
             int RightSumValue = 0;
-            
+
             /// Get right sum total.
             for (int i = 0; i < A.Length; i++)
             {
@@ -46,21 +80,21 @@ namespace WinForm
             }
 
             /// Get left value and compare
-            for (int i = 0; i < A.Length; i++) 
+            for (int i = 0; i < A.Length; i++)
             {
                 int CurrentValue = RightSumValue - A[i];
                 if (LeftSumValue == CurrentValue)
                 {
                     /// found equilibrium
-                    List_ReturnInt.Add(i);
+                    return i;
                 }
-                else 
+                else
                 {
                     LeftSumValue += A[i];
                     RightSumValue = CurrentValue;
                 }
             }
-            return List_ReturnInt.ToArray();
+            return -1;
         }
     }
 }
