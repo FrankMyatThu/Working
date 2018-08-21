07a40b96272c3829876591a5a0600992750ebe32
FrankMyatThu <myatthu1986@gmail.com>
2018-08-21 16:53:15 +0800

    NumberSorting Algo

---
+++ Converter/AlgorithmTesterProject/AlgorithmTesterProject/WinForm/NumberSorting.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm
+{
+    public partial class NumberSorting : Form
+    {
+        public NumberSorting()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberSorting_Load(object sender, EventArgs e)
+        {
+            int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+            bubbleSort(arr, arr.Length);
+
+            Console.WriteLine("Sorted array : ");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+        }
+
+        /// <summary>
+        /// https://www.geeksforgeeks.org/recursive-bubble-sort/
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="n"></param>
+        void bubbleSort(int[] arr,int n)
+        {
+            // Base case
+            if (n == 1)
+                return;
+
+            // One pass of bubble 
+            // sort. After this pass,
+            // the largest element
+            // is moved (or bubbled) 
+            // to end.
+            for (int i = 0; i < n - 1; i++)
+                if (arr[i] > arr[i + 1])
+                {
+                    // swap arr[i], arr[i+1]
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+
+            // Largest element is fixed,
+            // recur for remaining array
+            bubbleSort(arr, n - 1);
+        }
+
+
+    }
+}
--- Converter/AlgorithmTesterProject/AlgorithmTesterProject/WinForm/Program.cs
+++ Converter/AlgorithmTesterProject/AlgorithmTesterProject/WinForm/Program.cs
@@ -16,7 +16,7 @@ namespace WinForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GenerateRandomNumber());
+            Application.Run(new NumberSorting());
         }
     }
 }
